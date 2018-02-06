40773770ada858a647345293716d78f378f6aeb0
Shinya Uryu <suika1127@gmail.com>
2018-02-07 07:32:34 +0900

    execute remote driver in local

--- R/file_download.R
+++ R/file_download.R
@@ -29,22 +29,19 @@
 #' download_estat_excel(remDr, url = "https://www.e-stat.go.jp/stat-search/files?\n
 #' page=1&layout=datalist&toukei=00500226&tstat=000001015623&cycle=7\n
 #' &tclass1=000001032194&tclass2=000001100735&stat_infid=000031571891")
-#' 
-#' remDr <- remoteDriver(
-#' port = 4445L,
-#' browserName = "phantomjs",
-#' extraCapabilities = eCaps)
 #' }
 download_estat_excel <- function(driver, url) {
   
+  # ruuning at locac env
   # RSelenium::rsDriver(port = 4444L)
+  
   driver$open(silent = TRUE)
   
   driver$navigate(url)
-  webElem <- driver$findElement("css selector", "div.stat-search_result-list.js-items > a")
-  dataset_url <- webElem$getElementAttribute(attrName = "href")[[1]]
+  # webElem <- driver$findElement("css selector", "div.stat-search_result-list.js-items > a")
+  # dataset_url <- webElem$getElementAttribute(attrName = "href")[[1]]
   
-  driver$navigate(dataset_url)
+  # driver$navigate(dataset_url)
   webElem <- driver$findElement("css selector", "div.stat-dl_icon.stat-xls_icon.js-dl")
   childElems <- webElem$findChildElements("css selector", "span")
 
